8c60760e186c1853f2689b8545beaa5b79206c26
Kane Robinson <kane@kanestrades.com>
2020-05-12 23:43:32 +0800

    set up input questions in index.js

--- index.js
+++ index.js
@@ -4,16 +4,53 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 const axios = require("axios");
 
+// Input questions
 
-const questions = [
+// Badge
+// Description
+// Table of Contents
+// Installation
+// Usage
+// License
+// Contributing
+// Tests
+// Questions
+    // User GitHub profile picture
+    // User GitHub email
 
-];
+inquirer.prompt([
+    
+    {
+        type:"input",
+        name:"Badge 1",
+        message:"What is Badge 1?"
+    },
 
-function writeToFile(fileName, data) {
-}
+    {
+        type:"input",
+        name:"Badge 2",
+        message:"What is Badge 2?"
+    },
+    
+ 
+    {
+        type:"input",
+        name:"users name",
+        message:"what is your name?"
+    },
 
-function init() {
 
-}
+    {
+        type:"list",
+        name:"kanesList",
+        message:"This is a list",
+        choices:["list 1","list 2","list 3","list 4"]
+    },
 
-init();
+  
+    {
+        type:"checkbox",
+        name:"kanesCheckbox",
+        message:"This is a checkbox",
+        choices:["checkbox 1","checkbox 2","checkbox  3","checkbox  4"]
+    }])
